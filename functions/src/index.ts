2fbb555c41ffe0afef6443cf39eccfbf7ce3a799
yuuto52925goal <yuuto52925goal@gmail.com>
2024-11-28 11:34:48 -0700

    save to fire store

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,44 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
 import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+// import {getStorage} from "firebase-admin/storage";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-export const helloWorld = onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+interface requestType {
+  uri: string
+  uid: string
+  date: string
+  time: string
+}
+
+export const outFit = onRequest(async (request, response) => {
+  try {
+    const {uri, uid, time, date}: requestType = request.body;
+
+    const db = await getFirestore()
+      .collection(`users/${uid}/outfit`)
+      .add({
+        outfit: uri,
+        time: time,
+        date: date,
+      });
+    const output = `${db.id} at ${uid}`;
+    response.json({result: output});
+  } catch (e: unknown) {
+    console.error("Error:", (e as Error).message);
+    const error = e as { message: string; code?: number };
+    if (error.message.includes("App Check")) {
+      response.status(403).send("App Check validation error");
+    } else if (error.code === 7) {
+      response.status(403).send("IAM or Firestore Rules");
+    } else {
+      response.status(500).send("Unexpected error occurred");
+    }
+  }
 });
+
+
